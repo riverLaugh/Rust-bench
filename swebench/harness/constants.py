9daa3fe1e3b33b1742bac6d948ee89d9ef85c1f0
riverLaugh <2314398305@qq.com>
2024-11-23 19:34:01 +0800

    constant

--- swebench/harness/constants.py
+++ swebench/harness/constants.py
@@ -215,8 +215,7 @@ SPECS_ARROW = {
         "rustc": "1.81.0",
         "test_cmd":TEST_CARGO,
         "pre_install":[
-                        r"""sed -i 's|proc-macro2 = { version = "=1.0.47"|proc-macro2 = { version = "=1.0.75"|g' ./arrow-flight/Cargo.toml""",
-
+            r"""sed -i 's|proc-macro2 = { version = "=1.0.47"|proc-macro2 = { version = "=1.0.75"|g' ./arrow-flight/Cargo.toml""",
             r"""git submodule update --init""",
         ],
         "env_setup":[
