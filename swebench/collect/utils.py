fee05c6a5ee048536d0f4483da23d14171030d82
riverLaugh <2314398305@qq.com>
2024-12-07 15:22:01 +0800

    1

--- swebench/collect/utils.py
+++ swebench/collect/utils.py
@@ -355,16 +355,16 @@ def extract_patches(pull: dict, repo: Repo) -> tuple[str, str]:
     patch_test = ""
     patch_fix  = ""
     for hunk in PatchSet(patch):
-        # if any(
-        #     test_word in hunk.path for test_word in
-        #     ['test', 'tests', 'e2e', 'testing']
-        # ):
-        #     patch_test += str(hunk)
-        # else:
-        #     patch_fix += str(hunk)
-        patch_fix += str(hunk)
-    # return patch_fix, patch_test
-    return patch_fix
+        if any(
+            test_word in hunk.path for test_word in
+            ['test', 'tests', 'e2e', 'testing']
+        ):
+            patch_test += str(hunk)
+        else:
+            patch_fix += str(hunk)
+        # patch_fix += str(hunk)
+    return patch_fix, patch_test
+    # return patch_fix
 
 
 ### MARK: Repo Specific Parsing Functions ###
