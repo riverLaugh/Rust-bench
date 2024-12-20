53fbb2f39380d75b3c0c734fa7c1504e6e6b6458
Xihua Wang <2201174299@qq.com>
2024-12-20 13:38:57 +0800

    更新hyper constants

--- swebench/harness/make_test_cmds.py
+++ swebench/harness/make_test_cmds.py
@@ -8,17 +8,54 @@ from swebench.harness.constants import (
 from swebench.harness.utils import findCrate
 
 
-def _default_test_commands(
-    tests_changed: list[str],
-    rust_flags: Optional[str],
-    features: list[str] = None,
-    use_all_features: bool = False,
-    **kwargs,
+def _base_single_module_test_commands(
+        tests_changed: list[str],
+        rust_flags: Optional[str] = None,
+        rust_doc_flags: Optional[str] = None,
+        features: Optional[list[str]] = None,
+        use_all_features: bool = False,
+        before: Optional[list[str]] = None,
 ):
-    submodule_tests: dict[str, list | None] = {}
+    # generate specific str
+    features_str = f'--features="{" ".join(features)}" ' if features else ""
+    use_all_features_str = "--all-features " if use_all_features else ""
+    # generate cmds
+    cmds: list[str] = []
+    if before:
+        cmds.extend(before)
+    if rust_flags:
+        cmds.append(f'export RUSTFLAGS="{rust_flags}" ')
+    if rust_doc_flags:
+        cmds.append(f'export RUSTDOCFLAGS="{rust_doc_flags}"')
+    test_names: Optional[list[str]] = []
+    for test_path in tests_changed:
+        match = re.match(r"tests/([\w\-]+)\.rs", test_path)
+        if match:
+            test_names.append(match.group(1))
+        else:
+            test_names = None
+            break
+    if test_names:
+        cmds.extend(
+            f"cargo test --no-fail-fast {features_str}{use_all_features_str}--test {test_name}"
+            for test_name in test_names
+        )
+    else:
+        cmds.append(f"cargo test --no-fail-fast {features_str}{use_all_features_str}")
+    return cmds
+
+
+def _base_multi_module_test_commands(
+        tests_changed: list[str],
+        rust_flags: Optional[str] = None,
+        rust_doc_flags: Optional[str] = None,
+        features: Optional[list[str]] = None,
+        use_all_features: bool = False,
+):
+    submodule_tests: dict[str, Optional[list]] = {}
     for test_path in tests_changed:
         match = re.match(r"([\w\-]+)/tests/([\w\-]+)\.rs", test_path)
-        # integration test
+        # integration test  
         if match:
             submodule, test_name = match.group(1), match.group(2)
             if submodule not in submodule_tests:
@@ -29,12 +66,14 @@ def _default_test_commands(
         else:
             submodule_tests[test_path.split("/")[0]] = None
     # generate specific str
-    features_str = f'--features="{" ".join(features)}"' if features else ""
-    use_all_features_str = "--all-features" if use_all_features else ""
+    features_str = f'--features="{" ".join(features)}" ' if features else ""
+    use_all_features_str = "--all-features " if use_all_features else ""
     # generate cmds
-    cmds: list[str] = (
-        [f'export RUSTFLAGS="{rust_flags}"'] if rust_flags is not None else []
-    )
+    cmds: list[str] = []
+    if rust_flags:
+        cmds.append(f'export RUSTFLAGS="{rust_flags}" ')
+    if rust_doc_flags:
+        cmds.append(f'export RUSTDOCFLAGS="{rust_doc_flags}"')
     for submodule, test_names in submodule_tests.items():
         cmds.append(f"cd ./{submodule}")
         if isinstance(test_names, list):
@@ -51,7 +90,7 @@ def _default_test_commands(
 
 
 def make_arrow_rs_test_cmds(
-    instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
+        instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
 ):
     # ban cargo test warnning
     cmds = ['export RUSTFLAGS="-Awarnings"']
@@ -100,7 +139,7 @@ def make_arrow_rs_test_cmds(
 
 
 def make_asterinas_test_cmds(
-    instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
+        instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
 ):
     cmds = []
     test_crates = findCrate(tests_changed)
@@ -128,7 +167,7 @@ def make_asterinas_test_cmds(
 
 
 def make_tokio_test_cmds(
-    instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
+        instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
 ):
     # add allow lint deny
     options = [
@@ -142,17 +181,54 @@ def make_tokio_test_cmds(
         "-Arenamed_and_removed_lints",
         "-Abroken_intra_doc_links",
     ]
-    return _default_test_commands(
-        tests_changed=tests_changed, rust_flags=" ".join(options), use_all_features=True
+    return _base_multi_module_test_commands(
+        tests_changed=tests_changed,
+        rust_flags=" ".join(options),
+        use_all_features=True
     )
 
 
 def make_crossbeam_test_cmds(
-    instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
+        instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
 ):
-    return _default_test_commands(
+    return _base_multi_module_test_commands(
         tests_changed=tests_changed,
         rust_flags="-Awarnings",
+    )
+
+
+def make_hyper_test_cmds(
+        instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
+):
+    version = float(instance["version"])
+    if version > 1.0:
+        features = ['full', 'ffi', 'tracing']
+        rust_flags = '-A warnings --cfg hyper_unstable_tracing --cfg hyper_unstable_ffi'
+        rust_doc_flags = rust_flags
+        before = [
+            r"""lint_check_config="\n[lints.rust.unexpected_cfgs]\nlevel = \"warn\"\ncheck-cfg = ['cfg(hyper_unstable_tracing)', 'cfg(hyper_unstable_ffi)']\n" """,
+            r"""grep -q '\[lints\.rust\.unexpected_cfgs\]' ./Cargo.toml || echo -e "$lint_check_config" >> ./Cargo.toml""",
+        ]
+    else:
+        features = ['full', 'ffi']
+        rust_flags = '-A warnings --cfg hyper_unstable_ffi'
+        rust_doc_flags = rust_flags
+        before = [
+            r"cargo clean",
+            r"""lint_check_config="\n[lints.rust.unexpected_cfgs]\nlevel = \"warn\"\ncheck-cfg = ['cfg(hyper_unstable_ffi)']\n" """,
+            r"""grep -q '\[lints\.rust\.unexpected_cfgs\]' ./Cargo.toml || echo -e "$lint_check_config" >> ./Cargo.toml""",
+            r"""
+            find . -name "*.rs" -type f | while read file; do
+                sed -i 's/deny(warnings)/allow(warnings)/g' "$file"
+            done
+            """,
+        ]
+    return _base_single_module_test_commands(
+        tests_changed=tests_changed,
+        rust_flags=rust_flags,
+        rust_doc_flags=rust_doc_flags,
+        features=features,
+        before=before,
     )
 
 
@@ -161,11 +237,12 @@ MAP_REPO_TO_TESTS = {
     "asterinas/asterinas": make_asterinas_test_cmds,
     "tokio-rs/tokio": make_tokio_test_cmds,
     "crossbeam-rs/crossbeam": make_crossbeam_test_cmds,
+    "hyperium/hyper": make_hyper_test_cmds
 }
 
 
 def make_test_cmds(
-    instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
+        instance, specs, env_name, repo_directory, base_commit, test_patch, tests_changed
 ):
     repo = instance["repo"]
     if repo not in MAP_REPO_TO_TESTS:
