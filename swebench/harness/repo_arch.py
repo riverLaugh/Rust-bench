d8ee4c3748cd4e5a33b3d7f017b20a02927ee797
Xihua Wang <2201174299@qq.com>
2025-01-24 12:53:52 +0800

    更新repo_arch.py

--- swebench/harness/repo_arch.py
+++ swebench/harness/repo_arch.py
@@ -1,10 +1,50 @@
-import base64
 import os
-import re
-from typing import Optional
+from typing import Optional, Union, Callable
 
-import requests
+from func_timeout import func_timeout
+from func_timeout.exceptions import FunctionTimedOut
 from ghapi.all import GhApi
+from requests.exceptions import HTTPError
+
+
+def gh_tree(api: GhApi, owner: str, repo: str, commit: str = "HEAD", **kwargs):
+    return api.git.get_tree(owner, repo, commit, recursive=True, **kwargs)["tree"]
+
+
+class GithubApiPool:
+
+    def __init__(self, tokens: Union[str, list]):
+        if isinstance(tokens, str):
+            self.apis = [GhApi(token=token) for token in tokens.split()]
+        elif isinstance(tokens, list):
+            self.apis = [GhApi(token=token) for token in tokens]
+        else:
+            raise ValueError("tokens must be a str or list")
+
+    def call(self, *args, method: Callable, timeout: Optional[float] = None, **kwargs):
+        while True:
+            try:
+                return func_timeout(timeout, method, args=(self.fetch(), *args,), kwargs=kwargs)
+            except HTTPError as e:
+                if e.response.status_code == 403:
+                    print(f"403 Forbidden: Access denied, switching to next available API.")
+                    self._rotate()
+                else:
+                    print(f"HTTP Error {e.response.status_code}: {e.response.reason}. Retrying...")
+            except FunctionTimedOut:
+                print(f"Connection timed out. Retrying...")
+            except:
+                raise
+
+    def _rotate(self):
+        if not self.apis:
+            raise RuntimeError("No GitHub API available")
+        self.apis.append(self.apis.pop(0))
+
+    def fetch(self):
+        if not self.apis:
+            raise RuntimeError("No GitHub API available")
+        return self.apis[0]
 
 
 class RepoArchitecture:
@@ -14,16 +54,7 @@ class RepoArchitecture:
         self.parent: Optional[RepoArchitecture] = parent
         self.files: set[str] = set()
         self.dirs: dict[str, RepoArchitecture] = dict()
-        self.cargo_toml: Optional[str] = None
-
-    def has_dir(self, name: str) -> bool:
-        return name in self.dirs
-
-    def get_dir(self, name: str):
-        return self.dirs[name]
-
-    def has_file(self, name: str):
-        return name in self.files
+        self.is_module: bool = False
 
     def find_dir(self, paths: list[str], create_dir: bool = True):
         current = self
@@ -35,8 +66,15 @@ class RepoArchitecture:
                     current.dirs[path] = RepoArchitecture(path, current)
                     current = current.dirs[path]
                 else:
-                    return None
+                    raise RuntimeError(f"Path: {path} is not a directory")
         return current
+
+    def find_module(self, paths: list[str]):
+        module = self.find_dir(paths)
+        while not module.is_module:
+            module = module.parent
+        assert module is not None
+        return module
 
     def get_full_path(self):
         current, path = self.parent, self.name
@@ -47,60 +85,13 @@ class RepoArchitecture:
         return path
 
 
-def _ghapi(token: str) -> GhApi:
-    session = requests.Session()
-    session.request = lambda *args, **kwargs: requests.request(
-        *args, timeout=4, **kwargs
-    )
-    return GhApi(token=token, session=session)
-
-
-def _ghapi_info(
-    api: GhApi, owner: str, repo: str, path: str, commit: Optional[str] = None
-):
-    while True:
-        try:
-            return api.repos.get_content(owner, repo, path, ref=commit)
-        except (KeyboardInterrupt, SystemExit) as e:
-            raise e
-        except:
-            pass
-
-
-def _ghapi_file(
-    api: GhApi, owner: str, repo: str, path: str, commit: Optional[str] = None
-):
-    return base64.b64decode(
-        _ghapi_info(api, owner, repo, path, commit)["content"]
-    ).decode()
-
-
-def _ghapi_tree(api: GhApi, owner: str, repo: str, commit: str = "HEAD", max_retries=5):
-    retries = 0
-    while retries < max_retries:
-        try:
-            # print(f"Getting tree: {owner}/{repo}/{commit}")
-            return api.git.get_tree(owner, repo, commit, recursive=True)["tree"]
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except Exception as e:
-            retries += 1
-            print(f"Attempt {retries}/{max_retries} failed with error: {e}")
-            if retries >= max_retries:
-                raise RuntimeError("Max retries reached") from e
-
-
 def get_repo_arch(
-    token: str, owner: str, repo: str, commit: Optional[str] = None
+        pool: GithubApiPool, owner: str, repo: str, commit: Optional[str] = None
 ) -> RepoArchitecture:
-    api = _ghapi(token)
+    root = RepoArchitecture()
 
-    root = RepoArchitecture(None)
-
-    for item in _ghapi_tree(api, owner, repo, commit):
-        # print(item)
+    for item in pool.call(owner, repo, commit, method=gh_tree, timeout=5):
         item_type, item_path = item["type"], item["path"]
-
         paths = item_path.split("/")
 
         if item_type == "tree":
@@ -111,57 +102,25 @@ def get_repo_arch(
             paths, name = paths[:-1], paths[-1]
             arch = root.find_dir(paths)
             if name == "Cargo.toml":
-                arch.cargo_toml = _ghapi_file(api, owner, repo, item_path, commit)
+                arch.is_module = True
             elif name.endswith(".rs"):
                 arch.files.add(name)
 
     return root
 
 
-def get_repo_version(
-    repo: str,
-    root: RepoArchitecture,
-    default_version: Optional[str] = None,
-) -> str:
-    def _extract_version(_content: Optional[str]):
-        if not _content:
-            return None
-        m = re.search(r'^\s*version\s*=\s*"\s*(\S*)\s*"\s*$', _content)
-        if m:
-            return m.group(1)
-        else:
-            return None
-
-    version = _extract_version(root.cargo_toml)
-    if version:
-        return version
-    queue: list[RepoArchitecture] = [root]
-    while queue:
-        arch = queue.pop(0)
-        if arch.name == repo:
-            version = _extract_version(arch.cargo_toml)
-            if version:
-                return version
-        queue.extend(arch.dirs.values())
-    return default_version
-
-
 def get_cargo_test_cmd(
-    root: RepoArchitecture, tests: list[str], flags: Optional[str] = None
+        root: RepoArchitecture, tests: list[str], flags: Optional[str] = None
 ) -> list[str]:
     submodule_test_dict = dict()
     for test_path in tests:
         # assert .rs file
         if not test_path.endswith(".rs"):
             continue
-        test_dir = test_path.split("/")
-        test_dir, test_file = test_dir[:-1], test_dir[-1]
-        test_name = test_file.removesuffix(".rs")
+        test_segments = test_path.split("/")
+        test_dir, test_file, test_name = test_segments[:-1], test_segments[-1], test_segments[-1].removesuffix(".rs")
         # find nearest cargo submodule
-        module = root.find_dir(test_dir)
-        while not module.cargo_toml:
-            module = module.parent
-        module_path = module.get_full_path()
+        module_path = root.find_module(test_dir).get_full_path()
         # init submodule tests
         if module_path not in submodule_test_dict:
             submodule_test_dict[module_path] = list()
@@ -208,123 +167,68 @@ def get_cargo_test_cmd(
     return cmds
 
 
-def test_bitflags_1(root):
-    print(
-        get_cargo_test_cmd(
-            root,
-            ["tests/basic.rs"],
-        )
-    )
-
-
-def test_bitflags_2(root):
-    print(
-        get_cargo_test_cmd(
-            root,
-            ["tests/basic.rs", "tests/compile-fail/redefined.rs"],
-        )
-    )
-
-
-def test_bitflags_3(root):
-    print(
-        get_cargo_test_cmd(
-            root,
-            [
-                "tests/basic.rs",
-                "tests/compile-fail/redefined.rs",
-                "tests/compile-fail/trait/custom_impl.rs",
-            ],
-        )
-    )
-
-
-def test_bitflags_4(root):
-    print(
-        get_cargo_test_cmd(
-            root,
-            [
-                "tests/basic.rs",
-                "tests/compile-fail/redefined.rs",
-                "tests/compile-fail/trait/custom_impl.rs",
-                "tests/smoke-test/src/main.rs",
-            ],
-        )
-    )
-
-
-def test_bitflags_5(root):
-    print(
-        get_cargo_test_cmd(
-            root,
-            [
-                "tests/basic.rs",
-                "tests/compile-fail/redefined.rs",
-                "tests/compile-fail/trait/custom_impl.rs",
-                "tests/smoke-test/src/main.rs",
-                "src/lib.rs",
-            ],
-        )
-    )
-
-
 def test_bitflags():
-    root = get_repo_arch(
-        os.environ["GITHUB_TOKENS"],
-        "bitflags",
-        "bitflags",
-        "dc971042c8132a5381ab3e2165983ee7f9d44c63",
-    )
-    print(get_repo_version("bitflags", root, "0.0.0"))
-    test_bitflags_1(root)
-    test_bitflags_2(root)
-    test_bitflags_3(root)
-    test_bitflags_4(root)
-    test_bitflags_5(root)
-
-
-def test_arrow_1(root):
-    print(get_cargo_test_cmd(root, ["arrow-flight/tests/flight_sql_client.rs"]))
-
-
-def test_arrow_2(root):
-    print(
-        get_cargo_test_cmd(
-            root,
-            [
-                "arrow-flight/tests/flight_sql_client.rs",
-                "arrow-flight/examples/server.rs",
-            ],
-        )
-    )
-
-
-def test_arrow_3(root):
-    print(
-        get_cargo_test_cmd(
-            root,
-            [
-                "arrow-flight/tests/flight_sql_client.rs",
-                "arrow-flight/examples/server.rs",
-                "testing/data/csv/README.md",
-            ],
-        )
-    )
+    test_cases = [
+        ["tests/basic.rs"],
+        ["tests/basic.rs", "tests/compile-fail/redefined.rs"],
+        [
+            "tests/basic.rs",
+            "tests/compile-fail/redefined.rs",
+            "tests/compile-fail/trait/custom_impl.rs",
+        ],
+        [
+            "tests/basic.rs",
+            "tests/compile-fail/redefined.rs",
+            "tests/compile-fail/trait/custom_impl.rs",
+            "tests/smoke-test/src/main.rs",
+        ],
+        [
+            "tests/basic.rs",
+            "tests/compile-fail/redefined.rs",
+            "tests/compile-fail/trait/custom_impl.rs",
+            "tests/smoke-test/src/main.rs",
+            "src/lib.rs",
+        ]
+    ]
+    root = get_repo_arch(GithubApiPool(tokens=os.environ["GITHUB_TOKENS"]), "bitflags", "bitflags",
+                         "dc971042c8132a5381ab3e2165983ee7f9d44c63")
+    for test_case in test_cases:
+        print(get_cargo_test_cmd(root, test_case))
 
 
 def test_arrow():
-    root = get_repo_arch(
-        os.environ["GITHUB_TOKENS"],
-        "apache",
-        "arrow-rs",
-        "9ffa06543be51613ea1f509e63f6e7405b7d9989",
-    )
-    print(get_repo_version("arrow", root, "0.0.0"))
-    test_arrow_1(root)
-    test_arrow_2(root)
-    test_arrow_3(root)
+    test_cases = [
+        ["arrow-flight/tests/flight_sql_client.rs"],
+        [
+            "arrow-flight/tests/flight_sql_client.rs",
+            "arrow-flight/examples/server.rs",
+        ],
+        [
+            "arrow-flight/tests/flight_sql_client.rs",
+            "arrow-flight/examples/server.rs",
+            "testing/data/csv/README.md",
+        ]
+    ]
+    root = get_repo_arch(GithubApiPool(tokens=os.environ["GITHUB_TOKENS"]), "apache", "arrow-rs",
+                         "9ffa06543be51613ea1f509e63f6e7405b7d9989")
+    for test_case in test_cases:
+        print(get_cargo_test_cmd(root, test_case))
+
+
+def test_repo():
+    test_bitflags()
+    test_arrow()
+
+
+def test_rate():
+    pool = GithubApiPool(tokens=os.environ["GITHUB_TOKENS"])
+    cnt = 0
+    while True:
+        pool.call("bitflags", "bitflags", method=gh_tree, timeout=3)
+        print(f"Count: {cnt}")
+        cnt += 1
 
 
 if __name__ == "__main__":
-    # test_bitflags()
-    test_arrow()
+    # test_repo()
+    test_rate()
